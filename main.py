a557bcce84d163d88a758596e106db6a3b1f10b3
Jeremy Dothager <jeremydothager@gmail.com>
2017-02-10 01:38:59 -0600

    added comments and finished single post functionality

--- main.py
+++ main.py
@@ -25,31 +25,37 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 
+# create class to handle rendering the templates
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
-    
+
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
-    
+
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+
+# create Post database
 class Post(db.Model):
     title = db.StringProperty(required=True)
     post = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
 
+
+# handles the front page, displaying 5 newest posts
 class MainPage(Handler):
     def render_front(self, title="", post="", error=""):
         posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
-
         self.render("blog.html", title=title, post=post, error=error, posts=posts)
 
     def get(self):
         self.render_front()
 
+
+# handles new post functions
 class NewPost(Handler):
     def render_newpost(self, title="", post="", error=""):
         self.render('newpost.html', title=title, post=post, error=error)
@@ -68,19 +74,19 @@ class NewPost(Handler):
         else:
             a = Post(title=title, post=post)
             a.put()
-            self.redirect("/blog")
+            self.redirect("/blog/" + str(a.key().id()))
 
-            
-    
+
+# handles requests to view a single post
 class ViewPostHandler(Handler):
     def get(self, id):
         post = Post.get_by_id(int(id))
 
         if post:
-            self.render_viewpost(post)
+            self.render("singlepost.html", post=post)
         else:
             self.render("blog.html", error="That post ID doesn't exist")
-            
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
