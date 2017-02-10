2d6a6867470d8b08326ad742d473cd87a802b426
Jeremy Dothager <jeremydothager@gmail.com>
2017-02-09 18:46:15 -0600

    added html templates

--- main.py
+++ main.py
@@ -14,12 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+# setup jinja2
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Post(db.Model):
+    title = db.StringProperty(required=True)
+    post = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+class MainPage(Handler):
+    def render_front(self, title="", post="", error=""):
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
+
+        self.render("frontpage.html", title=title, post=post, error=error, posts=posts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+    
+    def post(self):
+        title = self.request.get("title")
+        post = self.request.get("post")
+
+        if title and post:
+            a = Post(title=title, post=post)
+            a.put()
+            
+            self.redirect("/")
+
+        else:
+            error = "we need both a title and some content!"
+            self.render_front(title=title, post=post, error=error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
